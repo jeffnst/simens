9c611edabec5d338551e70a464eef5c1d1ebdc3b
blackbone23 <rully.lukman@gmail.com>
2015-11-16 14:53:24 +0800

    add more desc index.php

--- index.php
+++ index.php
@@ -27,6 +27,24 @@
 </ol>
 
 </div>
+
+<div>
+<p>Sub Bagian Umum dan Kepegawaian, mempunyai tugas :</p>
+<ol>
+    <li>melakukan kegiatan ketatausahaan yang meliputi surat-menyurat, pengetikan, penggandaan dan pengelolaan kearsipan sesuai petunjuk teknis administasi perkantoran</li>
+    <li>memberikan pelayanan alat-alat tulis kantor dan perlengkapan lainnya, urusan keprotokolan dan pelayanan tamu terhadap satuan organisasi di lingkungan badan pengelolaan keuangan dan aset daerah</li>
+    <li>memelihara dan merawat gedung dan barang inventaris kantor serta membuat daftar dan laporan barang inventaris kantor</li>
+    <li>memelihara kebersihan, keapikan dan kerapian ruangan kantor serta kebersihan halaman kantor</li>
+    <li>melaksanakan administrasi perjalanan dinas kepala badan dan pegawai yang diserahi tugas kedinasan guna menunjang kelancaran pelaksanaan tugas</li>
+    <li>menyiapkan ruangan untuk rapat, upacara dan pertemuan-pertemuan sesuai petunjuk pimpinan</li>
+    <li>mengumpulkan, mengolah dan mensistematisasikan data kepegawaian</li>
+    <li>melaksanakan urusan kepegawaian yang meliputi usul pengangkatan, pemberhentian/pensiun, mutasi, promosi, kenaikan gaji berkala, kenaikan pangkat, cuti, pembuatan Karis/Karsu/Karpeg, Taspen, asuransi, Daftar Urut Kepangkatan (DUK), nominatif Daftar Struktur Pegawai (DSP) dan Daftar Penilaian Pelaksanaan Pekerjaan (DP-3)</li>
+    <li>mengatur dan menyusun usul pendidikan dan latihan pegawai</li>
+    <li>mengatur dan menyusun rekapitulasi absen pegawai serta menjaga disiplin pegawai</li>
+    <li>menyusun laporan kepegawaian berdasarkan ketentuan peraturan perundang-undangan yang berlaku</li>
+    <li>melaksanakan tugas lain sesuai bidang tugas dan tanggung jawabnya</li>
+</ol>
+</div>
 </div>
 
 <?php 
